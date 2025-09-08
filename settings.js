d1b6a53780ab2fd371c9359bb730e664e5727bf2
alongbots <aosiblings10@gmail.com>
2025-09-08 10:26:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 module.exports = {
   //Enter your session id here (optional)
-  SESSION_ID: process.env.SESSION_ID || '',
+  SESSION_ID: process.env.SESSION_ID || 'CYPHER-X:~UEsDBBQAAAgIAE6GJ1shgkevTAQAAG4HAAAKAAAAY3JlZHMuanNvbpVUXZOiOBT9L3nVGgFBxKquGkQFVBC*ha15CBAgyFcnQRun*O9baPf0POzO9vIUktS55557Tn6CosQULVADRj9BRfAFMtQuWVMhMALjOooQAV0QQgbBCAwmwetqsBvq5u4QzU55bntUxcLYcZaqaDH2mlZi+mrqZDZ8AfcuqGo*w8EfACd9jsmKceqYh*lqNo4cW3IKhBe1LqK5FVRkVcVD0*VOB*UF3FtEiAku4mmVoBwRmC1Q40BMvkaf6u7YjAQpryz3irTAEUgk5ObS1XRtLbviVc84H6Nsnq2*Rv82OBWazYmvEis7+qsVeKdIRsPQTea9zQpbxRUrSsVZN+H6pE9xXKDQDFHBMGu+rPvC8NaG56mzwz52dC4P5waedeJmlQdBRJr9TbpVtVrsqBF*jXgaKMF65u03b6sm1Bjj1IG9SHyUjROvihIhiYnBTZeS86H7k7hDPrxy*j+6a9p60lf6l2DPJdLlcmwInc4VTz2bMFW3qavn8jQTDaEqyi*SFyxLnsBmvZveUu8wWQe4jgwq7a8Vtac1RVmqDvjp6joJPulDVpM*sTR531yrOXMPYm*xZovyhF+cFWmxDf3OMFL2655kJ6tFOhj2zivvtogMf18TB*djJzzWJLQUmfNv+RFeSsNMBha7cFISvzw6OqPGDMGIv3cBQTGmjECGy+KxJwtdAMPLFgUEsYe8YCoY4sExKyzMj+wwW57EzIW2Em8XsJiceoupKjNFt00xi19AF1SkDBClKDQwZSVpLEQpjBEFo79+dEGB3thzcG25Pt8FESaU7Yu6ykoYfkz14xAGQVkXbNsUgdYuEAEj7nMbMYaLmLY61gUkQYIvSEsgo2AUwYyiXx0igkIwYqRGv1KrleFDeMd2D57ogS7IHwPBIRgBhZc5TlJkgRtKI+k7*XZtUWFVfSsQA12QPW4JAi*IMs*LsiRJsjKSvrf791*8WrgQMYgz2hrPwSp3MbWp1bsodK*rqhmrWqyCz34+jPEU*ubZXnY0kVOTPC2czdZxzweMtEP2VgYG3x*0j5MkjyS4vb78AwgYgYs1Vq693O*xOtyN07eNtulrwnG2ShQic32M1VriM1ckTZH4m2Vvrl5RCH0x0eaZfQ0O*oLs8uGswosTN0PKON7biXZ9aauF6IID9HuxFeTJbZ0HvrzSjXRKhZ4w8d0hv2G+xfnQ9utN9RqLVUpUTtNUf+UrseROReJN0+VQeLNcWezjXMy4WY57HWINs2WsPi37iEz2*lThh5naSbW*EUaP5Bewnd9*Tu7Ju*UXd+*+BvH+lPxLHMeBsDt6B2yk8qtUSjdunwrzmVvEmRdAY6yEHcbHold0jmcK7vcfXVBlkEUlycEIwCIkJQ5BF5Cybg1rFlH5pxdKNU3tvfEMUqZ+hmCHc0QZzKs2qpIsDPpDYfC85ZCyMiBNWgAxmtutoRu1qrYMso9IAbX95tIF3P8GUEsBAhQDFAAACAgAToYnWyGCR69MBAAAbgcAAAoAAAAAAAAAAAAAAICBAAAAAGNyZWRzLmpzb25QSwUGAAAAAAEAAQA4AAAAdAQAAAAA',
   
   //Enter your number here for administrative access to the bot
-  BOT_ADMIN: process.env.BOT_ADMIN || '254712345678',
+  BOT_ADMIN: process.env.BOT_ADMIN || '917005972085,918731810311',
   
   //Bot web server port
   PORT: parseInt(process.env.PORT) || 2605,
   
   //Enter your Github username here (Compulsory unless you have a valid premium key)
-  GITHUB_USERNAME: process.env.GITHUB_USERNAME || '',
+  GITHUB_USERNAME: process.env.GITHUB_USERNAME || 'alongbots',
   
   //Enter your desired bot password here. Users will be asked for this password when they try to connect to your bot via telegram or web
   //Must be numbers only and a total of 8 digits
